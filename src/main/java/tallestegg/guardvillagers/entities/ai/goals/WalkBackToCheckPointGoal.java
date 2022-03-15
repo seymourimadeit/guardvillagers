7781d00b2d6ff636e4d6717ed0fd74e96a455d14
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2022-03-15 15:51:21 -0700

    bumped version

--- src/main/java/tallestegg/guardvillagers/entities/ai/goals/WalkBackToCheckPointGoal.java
+++ src/main/java/tallestegg/guardvillagers/entities/ai/goals/WalkBackToCheckPointGoal.java
@@ -18,7 +18,7 @@ public class WalkBackToCheckPointGoal extends Goal {
 
     @Override
     public boolean canUse() {
-        return guard.getPatrolPos() != null && !this.guard.getPatrolPos().closerThan(this.guard.position(), 1.0D) && !guard.isFollowing() && guard.isPatrolling();
+        return guard.getPatrolPos() != null && !this.guard.getPatrolPos().closerThan(this.guard.blockPosition(), 1.0D) && !guard.isFollowing() && guard.isPatrolling();
     }
 
     @Override
@@ -31,8 +31,8 @@ public class WalkBackToCheckPointGoal extends Goal {
         BlockPos blockpos = this.guard.getPatrolPos();
         if (blockpos != null) {
             Vec3 vector3d = Vec3.atBottomCenterOf(blockpos);
-            Vec3 vector3d1 = DefaultRandomPos.getPosTowards(this.guard, 16, 3, vector3d, (double) ((float) Math.PI / 10F));
-            if (!this.guard.getPatrolPos().closerThan(this.guard.position(), 1.0D) || blockpos != null)
+            Vec3 vector3d1 = DefaultRandomPos.getPosTowards(this.guard, 16, 3, vector3d,  (float) Math.PI / 10F);
+            if (!this.guard.getPatrolPos().closerThan(this.guard.blockPosition(), 1.0D) || blockpos != null)
                 if (guard.getMainHandItem().getItem() instanceof ProjectileWeaponItem) {
                     this.guard.getNavigation().moveTo(blockpos.getX(), blockpos.getY(), blockpos.getZ(), this.speed);
                 } else if (vector3d1 != null && guard.getTarget() == null) {
