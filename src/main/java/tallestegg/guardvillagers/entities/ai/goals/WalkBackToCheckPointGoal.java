6f03c7d7219933fc1cf6df3e634a38e99d356d6e
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2023-08-05 11:16:37 -0700

    made all guards stand still on patrol point

--- src/main/java/tallestegg/guardvillagers/entities/ai/goals/WalkBackToCheckPointGoal.java
+++ src/main/java/tallestegg/guardvillagers/entities/ai/goals/WalkBackToCheckPointGoal.java
@@ -2,8 +2,6 @@ package tallestegg.guardvillagers.entities.ai.goals;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.ai.goal.Goal;
-import net.minecraft.world.entity.ai.util.DefaultRandomPos;
-import net.minecraft.world.item.ProjectileWeaponItem;
 import net.minecraft.world.phys.Vec3;
 import tallestegg.guardvillagers.entities.Guard;
 
@@ -32,13 +30,8 @@ public class WalkBackToCheckPointGoal extends Goal {
         BlockPos blockpos = this.guard.getPatrolPos();
         if (blockpos != null) {
             Vec3 vector3d = Vec3.atBottomCenterOf(blockpos);
-            Vec3 vector3d1 = DefaultRandomPos.getPosTowards(this.guard, 16, 3, vector3d,  (float) Math.PI / 10F);
             if (!this.guard.getPatrolPos().closerThan(this.guard.blockPosition(), 1.0D) || blockpos != null)
-                if (guard.getMainHandItem().getItem() instanceof ProjectileWeaponItem) {
-                    this.guard.getNavigation().moveTo(blockpos.getX(), blockpos.getY(), blockpos.getZ(), this.speed);
-                } else if (vector3d1 != null && guard.getTarget() == null) {
-                    this.guard.getNavigation().moveTo(vector3d1.x(), vector3d1.y(), vector3d1.z(), this.speed);
-                }
+                this.guard.getNavigation().moveTo(blockpos.getX(), blockpos.getY(), blockpos.getZ(), this.speed);
         }
     }
 }
