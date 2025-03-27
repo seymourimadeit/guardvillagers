494b06ad37ebcc3e7648f8dc169b1afea5831da2
sunbatheproductions <retiredjjj@gmail.com>
2025-03-27 15:31:52 -0400

    Update GuardEntityType.java

--- src/main/java/tallestegg/guardvillagers/GuardEntityType.java
+++ src/main/java/tallestegg/guardvillagers/GuardEntityType.java
@@ -1,6 +1,8 @@
 package tallestegg.guardvillagers;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -10,5 +12,10 @@ import tallestegg.guardvillagers.common.entities.Guard;
 //@Mod.EventBusSubscriber(modid = GuardVillagers.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class GuardEntityType {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(Registries.ENTITY_TYPE, GuardVillagers.MODID);
-    public static final DeferredHolder<EntityType<?>, EntityType<Guard>> GUARD = ENTITIES.register("guard", () -> EntityType.Builder.of(Guard::new, MobCategory.MISC).sized(0.6F, 1.90F).setShouldReceiveVelocityUpdates(true).build(GuardVillagers.MODID + "guard"));
+    public static final DeferredHolder<EntityType<?>, EntityType<Guard>> GUARD = ENTITIES.register("guard", () -> EntityType.Builder.of(Guard::new, MobCategory.MISC).sized(0.6F, 1.90F).setShouldReceiveVelocityUpdates(true).build(prefix("guard")));
+
+
+    private static ResourceKey<EntityType<?>> prefix(String path) {
+        return ResourceKey.create(Registries.ENTITY_TYPE, ResourceLocation.fromNamespaceAndPath(GuardVillagers.MODID, path));
+    }
 }
