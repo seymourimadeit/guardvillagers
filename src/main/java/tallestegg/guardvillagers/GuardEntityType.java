780c01d44651b92e5449bd26f848c55587ec2d21
sunbatheproductions <retiredjjj@gmail.com>
2025-06-18 09:33:39 -0400

    Update GuardEntityType.java

--- src/main/java/tallestegg/guardvillagers/GuardEntityType.java
+++ src/main/java/tallestegg/guardvillagers/GuardEntityType.java
@@ -12,7 +12,7 @@ import tallestegg.guardvillagers.common.entities.Guard;
 //@Mod.EventBusSubscriber(modid = GuardVillagers.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class GuardEntityType {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(Registries.ENTITY_TYPE, GuardVillagers.MODID);
-    public static final Supplier<EntityType<?>, EntityType<Guard>> GUARD = ENTITIES.register("guard", () -> EntityType.Builder.of(Guard::new, MobCategory.MISC).sized(0.6F, 1.90F).setShouldReceiveVelocityUpdates(true).build(prefix("guard")));
+    public static final DeferredHolder<EntityType<?>, EntityType<Guard>> GUARD = ENTITIES.register("guard", () -> EntityType.Builder.of(Guard::new, MobCategory.MISC).sized(0.6F, 1.90F).setShouldReceiveVelocityUpdates(true).build(prefix("guard")));
 
 
     private static ResourceKey<EntityType<?>> prefix(String path) {
