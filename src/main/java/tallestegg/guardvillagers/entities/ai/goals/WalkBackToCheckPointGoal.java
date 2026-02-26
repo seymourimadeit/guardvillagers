8557c97498c18d39ccc4b3883074fcee442f9271
seymourimadeit <saulgoodman53434344@gmail.com>
2026-02-25 19:44:04 -0800

    - [CHANGE] hopefully reduces issues with guard pathfinding to patrol points (with help from hrmcngs)     - If guards are continually stuck they will simply pause their patrol finding ai for a moment

--- src/main/java/tallestegg/guardvillagers/entities/ai/goals/WalkBackToCheckPointGoal.java
+++ src/main/java/tallestegg/guardvillagers/entities/ai/goals/WalkBackToCheckPointGoal.java
@@ -3,6 +3,7 @@ package tallestegg.guardvillagers.entities.ai.goals;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.util.DefaultRandomPos;
+import net.minecraft.world.level.pathfinder.Path;
 import net.minecraft.world.phys.Vec3;
 import tallestegg.guardvillagers.entities.Guard;
 
@@ -11,6 +12,8 @@ import java.util.EnumSet;
 public class WalkBackToCheckPointGoal extends Goal {
     private final Guard guard;
     private final double speed;
+    private long delayTime = 0L;
+
     public WalkBackToCheckPointGoal(Guard guard, double speedIn) {
         this.guard = guard;
         this.speed = speedIn;
@@ -19,21 +22,28 @@ public class WalkBackToCheckPointGoal extends Goal {
 
     @Override
     public boolean canUse() {
-        return guard.getTarget() == null && guard.getPatrolPos() != null && this.guard.blockPosition() != this.guard.getPatrolPos() && !guard.isFollowing() && guard.isPatrolling();
+        return guard.getTarget() == null && this.guard.getPatrolPos() != null && !this.guard.blockPosition().equals(this.guard.getPatrolPos()) && !guard.isFollowing() && guard.isPatrolling() && (guard.level().getGameTime() - delayTime) > 200L;
     }
 
     @Override
     public boolean canContinueToUse() {
-        return this.canUse() && this.guard.getNavigation().isInProgress();
+        return this.canUse() && this.guard.getNavigation().isInProgress() && (!this.guard.getNavigation().isStuck() || ((this.guard.getNavigation().getPath() != null && !(this.guard.getNavigation().getPath().getEndNode().distanceTo(guard.blockPosition()) > 2))));
     }
 
     @Override
     public void start() {
         BlockPos blockpos = this.guard.getPatrolPos();
         if (blockpos != null) {
-            Vec3 vector3d = Vec3.atCenterOf(blockpos);
-            this.guard.getNavigation().moveTo(vector3d.x, vector3d.y, vector3d.z, this.speed);
+            Path path = this.guard.getNavigation().createPath(blockpos, 0);
+            this.guard.getNavigation().moveTo(path, this.speed);
         }
+    }
+
+    @Override
+    public void stop() {
+        if (this.guard.getNavigation().getPath() != null && !this.guard.getNavigation().getPath().canReach())
+            this.delayTime = this.guard.level().getGameTime();
+        this.guard.getNavigation().stop();
     }
 
     @Override
