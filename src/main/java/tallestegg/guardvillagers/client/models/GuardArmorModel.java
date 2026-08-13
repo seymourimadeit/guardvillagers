7bfc136d7b46180403863af365e916d5d340da59
seymourimadeit <saulgoodman53434344@gmail.com>
2026-08-13 15:00:12 -0700

    [CHANGE] fix #309 bug where guards wearing armor had an animation glitch while eating food

--- src/main/java/tallestegg/guardvillagers/client/models/GuardArmorModel.java
+++ src/main/java/tallestegg/guardvillagers/client/models/GuardArmorModel.java
@@ -3,13 +3,13 @@ package tallestegg.guardvillagers.client.models;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
-import net.minecraft.client.model.geom.builders.*;
-import net.minecraft.client.model.object.armorstand.ArmorStandArmorModel;
-import net.minecraft.client.model.player.PlayerModel;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
 import net.minecraft.client.renderer.entity.ArmorModelSet;
-import tallestegg.guardvillagers.client.renderer.state.GuardRenderState;
 
-public class GuardArmorModel extends HumanoidModel<GuardRenderState> {
+public class GuardArmorModel extends GuardModel {
     public GuardArmorModel(ModelPart part) {
         super(part);
     }
@@ -21,14 +21,25 @@ public class GuardArmorModel extends HumanoidModel<GuardRenderState> {
     private static MeshDefinition createBaseMesh(CubeDeformation cubeDeformation) {
         MeshDefinition meshdefinition = HumanoidModel.createMesh(cubeDeformation, 0.0F);
         PartDefinition partdefinition = meshdefinition.getRoot();
-        partdefinition.addOrReplaceChild(
+        PartDefinition head = partdefinition.addOrReplaceChild(
                 "head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -10.0F, -4.0F, 8.0F, 8.0F, 8.0F, cubeDeformation), PartPose.ZERO
         );
+        head.addOrReplaceChild("nose",
+                CubeListBuilder.create().texOffs(54, 0).addBox(-1.0F, -1.0F, -2.0F, 2, 4, 2, new CubeDeformation(0.0F)),
+                PartPose.offset(0.0F, -3.0F, -4.0F));
         partdefinition.addOrReplaceChild(
                 "hat",
                 CubeListBuilder.create().texOffs(32, 0).addBox(-4.0F, -10.0F, -4.0F, 8.0F, 8.0F, 8.0F, cubeDeformation.extend(0.5F)),
                 PartPose.offset(0.0F, 1.0F, 0.0F)
         );
+        partdefinition.getChild("body").addOrReplaceChild("quiver", CubeListBuilder.create().texOffs(100, 0).addBox(-2.5F, -2.0F, 0.0F, 5, 10, 5,
+                new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.5F, 3.0F, 2.3F, 0.0F, 0.0F, 0.2617993877991494F));
+        partdefinition.getChild("left_arm").addOrReplaceChild("shoulderPad_right",
+                CubeListBuilder.create().texOffs(72, 33).mirror().addBox(0.0F, 0.0F, -3.0F, 5, 3, 6, new CubeDeformation(0.0F)),
+                PartPose.offsetAndRotation(-0.5F, -3.5F, 0.0F, 0.0F, 0.0F, 0.3490658503988659F));
+        partdefinition.getChild("right_arm").addOrReplaceChild("shoulderPad_left",
+                CubeListBuilder.create().texOffs(72, 33).addBox(-5.0F, 0.0F, -3.0F, 5, 3, 6, new CubeDeformation(0.0F)),
+                PartPose.offsetAndRotation(0.5F, -3.5F, 0.0F, 0.0F, 0.0F, -0.3490658503988659F));
         return meshdefinition;
     }
 
