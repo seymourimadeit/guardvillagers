f2492d153d9be7235c9eb5598fbb42920ed117d8
qq973700300 <973700300@qq.com>
2026-08-21 06:54:02 +0800

    Fix NeoForge configuration screen import

--- src/main/java/tallestegg/guardvillagers/GuardVillagers.java
+++ src/main/java/tallestegg/guardvillagers/GuardVillagers.java
@@ -14,7 +14,7 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
