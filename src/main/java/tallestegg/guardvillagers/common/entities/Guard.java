1897f99f6902059ff01947f2eacb0b93bf7d8d8d
seymourimadeit <saulgoodman53434344@gmail.com>
2026-08-20 21:50:26 -0700

    Update Guard.java

--- src/main/java/tallestegg/guardvillagers/common/entities/Guard.java
+++ src/main/java/tallestegg/guardvillagers/common/entities/Guard.java
@@ -1767,6 +1767,11 @@ public class Guard extends PathfinderMob implements CrossbowAttackMob, RangedAtt
             this.guard.getNavigation().stop();
             this.stop = false;
         }
+
+        @Override
+        public boolean requiresUpdateEveryTick() {
+            return true;
+        }
     }
 
     public static class GuardGroundPathNavigation extends GroundPathNavigation {
